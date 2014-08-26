aace9d3e6913c1e19b71ee135e3074bc32fed250
psykid <psykid@users.noreply.github.com>
2014-08-27 01:01:05 +0530

    simple login html form

---
+++ feedback-form/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+ <html> <head> <title>Sign-In</title> 
+ <link rel="stylesheet" type="text/css" href="style-sign.css"> </head>
+ 
+ <body id="body-color"> 
+ <div id="Sign-In"> 
+ <fieldset style="width:40%">
+ <legend>LOG-IN HERE</legend> 
+ <form method="POST" action="connectivity.php"> 
+ User <br><input type="text" name="user" size="40"><br> 
+ Password <br><input type="password" name="pass" size="40"><br> 
+ <input id="button" type="submit" name="submit" value="Log-In"> 
+ </form>
+</fieldset> 
+ </div> 
+ </body> 
+ 
+ </html> 
