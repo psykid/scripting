782f95dc400d93104b7b9d31a473f0455641e646
Mohana Preetam <preetamnaga5@gmail.com>
2016-04-27 23:42:37 +0530

    when the user logs in and checks his prev attempts

--- quiz/already_attempted.php
+++ quiz/already_attempted.php
@@ -30,33 +30,47 @@ if ($user != "") {
     while ($row = mysqli_fetch_assoc($result)) {
         //TODO: change the queries according to the new database.
         $q_id = $row['quizzId'];
-        $s_ans = $row['options_selected'];
-        $score = $row['score'];
 
-        $query1 = "select * from all_quiz where Q_ID='$q_id'";
-        $result2 = mysqli_query($con, $query1);
+        $query = "select * from quizzes where quizId='$q_id'";
+        $result2 = mysqli_query($con, $query);
         $row2 = mysqli_fetch_assoc($result2);
-        $quiz_name = $row2['quiz_name'];
-        echo "<h4>$quiz_name</h4><br>";
+        $quiz_name = $row2['description'];
+        echo "<h2>$quiz_name</h2><br>";
 
-        $query2 = "SELECT * FROM " . $quiz_name;
+        $query2 = "SELECT * FROM questions WHERE quizId = " .$q_id;
         $result3 = mysqli_query($con, $query2) or die(mysqli_error($con) . " in quiz name");
-        $i = 0;
         echo "<pre><h5>";
         while ($row3 = mysqli_fetch_assoc($result3)) {
-            $echo = $i + 1;
-            echo "$echo)  ";
-            $qstn = $row3['question'];
-
-            echo $qstn . "<br>";
-            if ($s_ans[$i] != '0') {
-                $ans = "option" . $s_ans[$i];
-                echo "your answer is " . $ans . " ";
-                echo "$row3[$ans]<br><br>";
-            } else {
-                echo "didn't answer $s_ans[$i]<br>";
+            $question = $row3['question'];
+            $ques_Id = $row3['quizId'];
+            //print the question
+            echo $question . "<br>";
+            $query_options = "SELECT * FROM answers WHERE questionId = " .$ques_Id;
+            $result_options = mysqli_query($con, $query_options) or die(mysqli_error($con));
+            //$user_answer
+            $query_user_ans = "SELECT answer FROM quiz_students WHERE quizzId = ".$q_id.
+                " and studentId = ".$uid. " and questionId = ".$ques_Id;
+            $result_user_ans = mysqli_query($con, $query_user_ans) or die(mysqli_error($con));
+            $row_user_answer = mysqli_fetch_assoc($result_user_ans);
+            $user_answer = $row_user_answer['answer'];
+            while ($row_options = mysqli_fetch_assoc($result3)) {
+                //TODO #5: have a better way of representing the cases:
+                //options available
+                $option = $row_options['text'];
+                $correctness = $row_options['isCorrect'];
+                if($user_answer == $option && $correctness ==1)
+                    //user selected correct option
+                    echo "<b><i>" .$option . "</b></i><br>";
+                elseif($user_answer == $option)
+                    //user selected a wrong option
+                    echo "<i>" .$option ."</i><br>";
+                elseif($correctness == 1)
+                    //this is the correct option
+                    echo "<b>".$option."</b><br>";
+                else
+                    //a wrong unselected option
+                    echo $option."<br>";
             }
-            $i++;
         }
         echo "</h5></pre>";
         echo "<br><br><br>";
